fa781c6e52f13eedb65e683a1cfdb2f13c9ad849
Vladimir Afanasiev <vladimir.afanasiev@scalewill.com>
2023-06-17 23:31:44 +0300

    add rollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import {
   BrowserRouter, Navigate, Outlet, Route, Routes, useLocation,
 } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
 import { ToastContainer } from 'react-toastify';
 import leoProfanity from 'leo-profanity';
 import { io } from 'socket.io-client';
+import { Provider as RollbarProvider, ErrorBoundary as ErrorBoundaryRollbar, LEVEL_ERROR } from '@rollbar/react';
 import Login from './pages/Login';
 import Error from './pages/Error';
 import useAuth from './hooks/useAuth';
@@ -34,6 +35,13 @@ const App = async () => {
   const ruDict = leoProfanity.getDictionary('ru');
   leoProfanity.add(ruDict);
 
+  const rollbarConfig = {
+    enabled: process.env.NODE_ENV === 'production',
+    accessToken: process.env.ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  };
+
   const i18n = i18next.createInstance();
 
   await i18n.use(initReactI18next).init({
@@ -44,29 +52,36 @@ const App = async () => {
   const socket = io('');
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <SocketProvider socket={socket}>
-          <AuthProvider>
-            <BrowserRouter>
-              <Modal />
-              <div className="d-flex flex-column h-100">
-                <Navbar />
-                <Routes>
-                  <Route path="login" element={<Login />} />
-                  <Route path="signup" element={<Registration />} />
-                  <Route path="" element={<PrivateRoute />}>
-                    <Route path="/" element={<ChatPage />} />
-                    <Route path="*" element={<Error />} />
-                  </Route>
-                </Routes>
-              </div>
-              <ToastContainer />
-            </BrowserRouter>
-          </AuthProvider>
-        </SocketProvider>
-      </Provider>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundaryRollbar
+        level={LEVEL_ERROR}
+        fallbackUI={<div>Что то пошло не так</div>}
+      >
+        <I18nextProvider i18n={i18n}>
+          <ReduxProvider store={store}>
+            <SocketProvider socket={socket}>
+              <AuthProvider>
+                <BrowserRouter>
+                  <Modal />
+                  <div className="d-flex flex-column h-100">
+                    <Navbar />
+                    <Routes>
+                      <Route path="login" element={<Login />} />
+                      <Route path="signup" element={<Registration />} />
+                      <Route path="" element={<PrivateRoute />}>
+                        <Route path="/" element={<ChatPage />} />
+                        <Route path="*" element={<Error />} />
+                      </Route>
+                    </Routes>
+                  </div>
+                  <ToastContainer />
+                </BrowserRouter>
+              </AuthProvider>
+            </SocketProvider>
+          </ReduxProvider>
+        </I18nextProvider>
+      </ErrorBoundaryRollbar>
+    </RollbarProvider>
   );
 };
 
